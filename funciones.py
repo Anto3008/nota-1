8ae62900944499485ada4991d61cf1e8240c1bf8
Anto3008 <antoniagaete2005@gmail.com>
2023-06-14 10:26:34 -0400

    hola

--- funciones.py
+++ funciones.py
@@ -1,3 +1,13 @@
-print("hola")
-print("anto")
-print("perro")
+def numero_primo(num):
+    if num == 1 or num == 0:
+        return False 
+    elif num == 2:
+        return True
+    elif num > 2:
+        for divisor in range(2, num):
+            if num % divisor == 0:
+                return False
+            elif num % divisor != 0 and divisor == num-1:
+                return True
+
+print()
